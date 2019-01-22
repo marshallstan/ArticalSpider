fd24628384cd54c829f27897ea5ae40ea11d6e70
marshall wu <marshall.wu@cssinco.cn>
2019-01-22 16:53:57 +0800

    jobbole content complete

--- ArticalSpider/spiders/jobbole.py
+++ ArticalSpider/spiders/jobbole.py
@@ -1,24 +1,48 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from scrapy.http import Request
+from urllib import parse
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/110287/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
+        post_urls = response.css('#archive .floated-thumb .post-thumb a::attr(href)').extract()
+        for post_url in post_urls:
+            print(post_url)
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+
+        next_url = response.css('.next.page-numbers::attr(href)').extract_first('')
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+
+    def parse_detail(self, response):
         titlt = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0]
         create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace('·', '').strip()
         praise_nums = response.xpath('//span[contains(@class, "vote-post-up")]/h10/text()').extract()[0]
         fav_nums = response.xpath('//span[contains(@class, "bookmark-btn")]/text()').extract()[0]
         match_re = re.match('.*?(\d+).*', fav_nums)
         if match_re:
-            fav_nums = match_re.group(1)
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
 
         comment_nums = response.xpath('//a[@href="#article-comment"]/span/text()').extract()[0]
         match_re = re.match('.*?(\d+).*', comment_nums)
         if match_re:
-            comment_nums = match_re.group(1)
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
+
+        content = response.xpath('//div[@class="entry"]').extract()[0]
+
+        tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith('评论')]
+        tags = ','.join(tag_list)
+
         pass
